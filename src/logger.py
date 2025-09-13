39f14f40f3e4c2aac97c672709d946d60e555c59
sonny <sonny@yeacreate.com>
2025-09-13 16:39:34 +0800

    update

---
+++ src/logger.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+
+"""
+--------------------------------------
+@File       : logger.py
+@Author     : maixiaochai
+@Email      : maixiaochai@outlook.com
+@CreatedOn  : 2020/11/23 13:50
+--------------------------------------
+"""
+from logging import getLogger, StreamHandler, Formatter, INFO, DEBUG, WARNING, ERROR, CRITICAL
+from colorama import Fore, Style, init  # 用于 Windows 颜色兼容
+from logging.handlers import RotatingFileHandler
+from os import makedirs
+from os.path import exists, join as path_join, dirname, basename
+
+
+init(autoreset=True)  # autoreset=True 会自动重置颜色，避免污染后续输出
+
+class Logger:
+    """封装的用于类的通用功能"""
+
+    def __init__(self, log_dir: str = None, filename: str = None, max_size: float or int = None, backup_count: int = None):  # type: ignore
+        """
+        :param log_dir:         保存日志的目录
+        :param filename:        日志名称，默认为该类所在的文件的 文件名.log
+        :param max_size:        单个日志文件最大大小，单位 MB
+        :param backup_count:    除名称为 filename的文件外, 备份日志的数量
+
+        说明：
+            1）默认只对级别 >= INFO 的日志才会进行log操作，可通过设置 self.log_level值来修改
+            2）其它更多更进一步的设置， 请在 "log参数设置" 区进行设置
+        """
+        # ===========================[ 处理参数值 ]===========================
+        log_dir = log_dir or path_join(dirname(__file__), 'logs')
+
+        filename = filename or basename(__file__)
+        self.filename = self.__check_log_suffix(filename)
+
+        # log 文件绝对路径
+        self.log_file_path = path_join(log_dir, self.filename)
+
+        max_size = max_size or 64
+        self.max_size = max_size * 1024**2
+
+        self.backup_count = backup_count or 8
+
+        # ===========================[ log参数设置 ]===========================
+        # 一些默认的设置
+        self.encoding = "utf-8"
+
+        # ≥ log_level 级别才会被log
+        self.log_level = INFO
+
+        # ≥ file_log_level 级别才会被记录到文件
+        self.file_log_level = INFO
+
+        # ≥ print_log_level 级别才会被打印到屏幕
+        self.print_level = INFO
+
+
+        # log_dir 目录，如果不存在则创建
+        self.__check_dirs(log_dir)
+
+        self.COLOR_MAP = {DEBUG: Fore.BLUE, INFO: Fore.GREEN, WARNING: Fore.YELLOW, ERROR: Fore.RED, CRITICAL: Fore.RED + Style.BRIGHT}  # 蓝色  # 绿色  # 黄色  # 红色  # 亮红色
+
+        
+        color = self.COLOR_MAP.get(self.print_level, Fore.RESET)
+
+        
+        # %(funcName)s，函数名
+        # %(filename)s:%(lineno)d，文件名:行号
+        self.formatter = f'{color}[ %(asctime)s ][ %(filename)s:%(lineno)d ][ %(levelname)s ] \n%(message)s'
+
+    def __get_logger(self):
+        formatter = Formatter(self.formatter)
+
+        # log文件
+        # rotating_file_handler = RotatingFileHandler(filename=self.log_file_path, maxBytes=self.max_size, backupCount=self.backup_count, encoding=self.encoding)
+        # rotating_file_handler.setLevel(self.file_log_level)
+        # rotating_file_handler.setFormatter(formatter)
+
+        # log print
+        stream_handler = StreamHandler()
+        stream_handler.setLevel(self.print_level)
+        stream_handler.setFormatter(formatter)
+        
+
+        logger = getLogger()
+
+        logger.setLevel(self.log_level)
+        logger.addHandler(stream_handler)
+        # logger.addHandler(rotating_file_handler)
+
+        return logger
+
+    @staticmethod
+    def __check_dirs(dir_path: str):
+        """检查目录是否存在，不存在则递归创建"""
+        if not exists(dir_path):
+            makedirs(dir_path)
+
+    @staticmethod
+    def __check_log_suffix(log_name: str) -> str:
+        """确保log_name是以'.log'"""
+        suffix = '.log'
+
+        if not log_name.endswith(suffix):
+            log_name = f"{log_name}{suffix}"
+
+        return log_name
+
+    @property
+    def log(self):
+        return self.__get_logger()
+
+
+myLogger = Logger()
+log = myLogger.log
---
+++ src/uni1.py
@@ -0,0 +1,37 @@
+from logger import log
+import os
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+
+
+def read_csv(path: str):
+    '''
+    读取csv文件
+    '''
+    return pd.read_csv(os.path.join(os.path.dirname(__file__), path))
+
+
+def show_data():
+    '''
+    展示数据
+    '''
+    train_csv = read_csv('../data/titanic/train.csv')
+    dftrain_raw = train_csv.head(10)
+    log.info(dftrain_raw)
+
+    ax = train_csv['Survived'].value_counts().plot(kind='bar', figsize=(12, 8), fontsize=15, rot=0)
+    ax.set_ylabel('Counts', fontsize=15)
+    ax.set_xlabel('Survived', fontsize=15)
+    plt.show()
+
+
+def main():
+    show_data()
+
+
+if __name__ == "__main__":
+    main()
